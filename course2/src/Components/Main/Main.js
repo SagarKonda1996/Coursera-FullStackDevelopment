529373860ad0994e1cfe49c88ba45630587e2f0b
sagar <sagarkonda596@gmail.com>
2020-05-04 03:24:50 +0530

    "Header and Footer"

--- course2/src/Components/Main/Main.js
+++ course2/src/Components/Main/Main.js
@@ -3,24 +3,21 @@ import { Navbar, NavbarBrand} from "reactstrap";
 import { DISHES } from "../../Shared/dishes";
 import MenuComponent from './MenuComponent';
 import DishdetailComponent from './DishdetailComponent';
+import Header from '../Header';
+import Footer from '../Footer';
 const Main = props => {
     const [dishes, setDishes] = useState(DISHES)
     const [selectedDish, setSelectedDish] = useState(null)
     return (
         <>
-        <Navbar dark color="primary">
-     <div className="container">
-       <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-     </div>
-     </Navbar>
-     <div>
+       <Header/>
             <div className="container">
                 <div className="row">
                 <MenuComponent dishes={dishes} onSelect={setSelectedDish}/>
                 </div>
                 <DishdetailComponent dish={selectedDish} />
             </div>
-        </div>
+        <Footer/>
      </>
     )
 }
