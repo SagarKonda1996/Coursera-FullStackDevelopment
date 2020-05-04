36732c75fcac7c6add36bf759935cc94c3daba6f
sagar <sagarkonda596@gmail.com>
2020-05-05 01:59:38 +0530

    Combining Reducers

--- course2/src/Redux/ConfigureStore.js
+++ course2/src/Redux/ConfigureStore.js
@@ -1,9 +1,32 @@
-import { createStore } from "redux";
+import { createStore,combineReducers } from "redux";
 import { Reducer,initialState } from "./Reducer";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotions";
+
+/*
+ Global Reducer
+*/
+
+// export const ConfigureStore=()=>{
+//     const store=createStore(
+//         Reducer,
+//         initialState
+//         )
+//     return store
+// }
+/*
+Combined Reducer
+*/
 export const ConfigureStore=()=>{
     const store=createStore(
-        Reducer,
-        initialState
-        )
+        combineReducers({
+            dishes:Dishes,
+            comments:Comments,
+            leaders:Leaders,
+            promotions:Promotions
+        })
+    )
     return store
 }
