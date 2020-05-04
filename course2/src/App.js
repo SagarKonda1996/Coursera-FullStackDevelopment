70b6a53bb9df7ebcf521c26a595e9cd4050b1aa6
sagar <sagarkonda596@gmail.com>
2020-05-05 00:19:24 +0530

    Redux Integrated

--- course2/src/App.js
+++ course2/src/App.js
@@ -5,11 +5,14 @@ import MenuComponent from './Components/MenuComponent';
 import { DISHES } from "./Shared/dishes";
 import Main from './Components/Main';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import {ConfigureStore} from './Redux/ConfigureStore'
 
 function App() {
   const [dishes, setDishes] = useState(DISHES)
   return (
-    <BrowserRouter>
+    <Provider store={ConfigureStore()}>
+        <BrowserRouter>
     <div className="App">
      {/* <Navbar dark color="primary">
      <div className="container">
@@ -20,6 +23,10 @@ function App() {
      <Main/>
     </div>
     </BrowserRouter>
+
+
+    </Provider>
+    
     
   );
 }
--- course2/src/Components/Main/Main.js
+++ course2/src/Components/Main/Main.js
@@ -1,55 +1,44 @@
 import React,{useState} from 'react'
-import { Navbar, NavbarBrand} from "reactstrap";
-// import { DISHES } from "../../Shared/dishes";
 import MenuComponent from '../MenuComponent/MenuComponentWeek2';
 import DishdetailComponent from '../DishdetailComponent/DishdetailWeek2';
 import Header from '../Header';
 import Footer from '../Footer';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect,withRouter } from 'react-router-dom';
 import Home from '../Home';
 import Contact from '../Contact';
-// import { DISHES,PROMOTIONS,LEADERS,COMMENTS } from "../../Shared/JSONDATA";
-import {DISHES} from '../../Shared/dishes'
-import {COMMENTS} from '../../Shared/comments'
-import {PROMOTIONS} from '../../Shared/promotions'
-import {LEADERS} from '../../Shared/leaders'
+import { connect } from "react-redux";
 import About from '../Aboutus';
-const Main = props => {
+const mapStateToProps=state=>{
+    return {
+        dishes:state.dishes,
+        comment:state.comments,
+        promotions:state.promotions,
+        leaders:state.leaders
+    }
+}
+const Main = ({dishes=[],promotions=[],comments=[],leaders=[]}) => {
     
-    const [dishes, setDishes] = useState(DISHES)
-    const [homePageData, setHomePageData] = useState({
-        dishes: DISHES,
-        comments: COMMENTS,
-        promotions: PROMOTIONS,
-        leaders: LEADERS
-    })
-    const [selectedDish, setSelectedDish] = useState(null)
+    
     const HomePage = () => {
         return(
             <Home 
-            dish={homePageData.dishes.filter((dish) => dish.featured)[0]}
-            promotion={homePageData.promotions.filter((promo) => promo.featured)[0]}
-            leader={homePageData.leaders.filter((leader) => leader.featured)[0]}
+            dish={dishes.filter((dish) => dish.featured)[0]}
+            promotion={promotions.filter((promo) => promo.featured)[0]}
+            leader={leaders.filter((leader) => leader.featured)[0]}
 
             />
         );
       }
       const DishWithId = ({match}) => {
         return(
-            <DishdetailComponent dish={homePageData.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-              comments={homePageData.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            <DishdetailComponent dish={dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
         );
       };
-      const AboutUs=()=><About leaders={homePageData.leaders}/>
+      const AboutUs=()=><About leaders={leaders}/>
     return (
         <>
        <Header/>
-            {/* <div className="container">
-                <div className="row">
-                <MenuComponent dishes={dishes} onSelect={setSelectedDish}/>
-                </div>
-                <DishdetailComponent dish={selectedDish} />
-            </div> */}
              <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/contactus' component={Contact} />} />
@@ -64,7 +53,7 @@ const Main = props => {
                         () =>
                           
                                     <MenuComponent
-                                        dishes={homePageData.dishes}
+                                        dishes={dishes}
                                         onSelect={() => { }}
                                     />
                              
@@ -80,4 +69,4 @@ const Main = props => {
 
 
 
-export default Main
+export default withRouter(connect(mapStateToProps)(Main));
