b19ba497f97b314cc24a3f1c2cf1ed9277f4b782
sagar <sagarkonda596@gmail.com>
2020-05-05 22:31:02 +0530

    "Fetch Handling Errors"

--- course2/src/Redux/ActionCreators.js
+++ course2/src/Redux/ActionCreators.js
@@ -16,8 +16,26 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes=()=>(dispatch)=>{
     dispatch(dishesLoading(true));
     return fetch(baseUrl+'dishes')
+    .then(response=>{
+        if(response.ok){
+            return response
+        }
+        else{
+            var error =new Error('Error ' + response.status + ' : '+ response.statusText)
+            error.response=response
+            throw error
+        }
+    },
+    error=>{
+        var err=new Error(error.message);
+        throw err
+    }
+    )
     .then(response=>response.json())
     .then(dishes=>dispatch(addDishes(dishes)))
+    .catch(error=>{
+        dispatch(dishesFailed(error.message))
+    })
 }
 
 
@@ -38,8 +56,26 @@ export const dishesFailed=(errMess)=>({
 //Fetching Comments
 export const fetchComments=()=>(dispatch)=>{
     return fetch(baseUrl+'comments')
+    .then(response=>{
+        if(response.ok){
+            return response
+        }
+        else{
+            var error =new Error('Error ' + response.status + ' : '+ response.statusText)
+            error.response=response
+            throw error
+        }
+    },
+    error=>{
+        var err=new Error(error.message);
+        throw err
+    }
+    )
     .then(response=>response.json())
     .then(comments=>dispatch(addComments(comments)))
+    .catch(error=>{
+        dispatch(commentsFailed(error.message))
+    })
 }
 
 
@@ -58,8 +94,26 @@ export const commentsFailed=(errMess)=>({
 export const fetchPromos=()=>(dispatch)=>{
     dispatch(promosLoading(true));
     return fetch(baseUrl+'promotions')
+    .then(response=>{
+        if(response.ok){
+            return response
+        }
+        else{
+            var error =new Error('Error ' + response.status + ' : '+ response.statusText)
+            error.response=response
+            throw error
+        }
+    },
+    error=>{
+        var err=new Error(error.message);
+        throw err
+    }
+    )
     .then(response=>response.json())
     .then(promos=>dispatch(addPromos(promos)))
+    .catch(error=>{
+        dispatch(promosFailed(error.message))
+    })
 }
 
 
--- course2/src/Redux/promotions.js
+++ course2/src/Redux/promotions.js
@@ -9,7 +9,7 @@ export const Promotions = (state = {
             return {...state,isLoading:true,errMess:null,promotions:[]}
         case ActionTypes.ADD_PROMOS:
             return {...state,isLoading:false,errMess:null,promotions:action.payload}
-        case ActionTypes.PROMOS_LOADING:
+        case ActionTypes.PROMOS_FAILED:
             return {...state,isLoading:false,errMess:action.payload,promotions:[]}
         default:
           return state;
