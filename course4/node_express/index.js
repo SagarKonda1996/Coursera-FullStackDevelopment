07e2424a9b35be1db54f049ba7147c797fb44d38
sagar <sagarkonda596@gmail.com>
2020-05-19 00:55:28 +0530

    Assignment 1

--- course4/node_express/index.js
+++ course4/node_express/index.js
@@ -2,73 +2,20 @@ const express=require('express')
 const http=require('http')
 const morgan=require('morgan')
 const bodyParser=require('body-parser')
+const dishRouter=require('./routes/dishRouter')
+const promotionsRouter=require('./routes/promoRouter')
+const leadersRouter=require('./routes/leaderRouter')
+
 const hostname='localhost'
 const port=3000
 
-// Initialize Express
-
 const app=express()
-
-/*
-Configuring Express App
-
-*/
-
-//Configuring the App to use Morgan for Logging
-
 app.use(morgan('dev'))
-
-//Configuring to use Body Parser to parse json data from the body
-
 app.use(bodyParser.json())
 
-// For /dishes End point
-
-app.all('/dishes',(req,res,next)=>{
-    res.statusCode=200
-    res.setHeader('Content-Type','text/plain')
-    next();
-    
-})
-//next Specifies to perform next possible operation 
-// If we modify the Req or Res from where it is called then the modified values will passed to next possible operation
-
-app.get('/dishes',(req,res,next)=>{
-    res.end("Will Send All Dishes to You")
-})
-
-app.post('/dishes',(req,res,next)=>{
-    res.end('will add the dish : '+ req.body.name +' '+req.body.description)
-})
-
-app.put('/dishes',(req,res,next)=>{
-    res.statusCode=403
-    res.end('PUT operation not Supported')
-})
-
-app.delete('/dishes',(req,res,next)=>{
-    res.end('Deleting All dishes')
-})
-
-//For /dishes/:dishId
-app.get('/dishes/:dishId',(req,res,next)=>{
-    res.end(`Will Send ${req.params.dishId} Dish details to You`)
-})
-
-app.post('/dishes/:dishId',(req,res,next)=>{
-    res.statusCode=403
-    res.end('POST Operation Not Supported')
-})
-
-app.put('/dishes/:dishId',(req,res,next)=>{
-    
-    res.end(`Will Update Dish with dish Id ${req.params.dishId} With Details Name = ${req.body.name} and Description = ${req.body.description}`)
-    
-})
-
-app.delete('/dishes/:dishId',(req,res,next)=>{
-    res.end(`Deleting  dish with ${req.params.dishId}`)
-})
+app.use('/dishes',dishRouter)
+app.use('/promotions',promotionsRouter)
+app.use('/leaders',leadersRouter)
 
 
 //Serve Files from Directory
