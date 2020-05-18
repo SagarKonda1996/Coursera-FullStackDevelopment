11cf0b4ee99bff28f1dcaf372a3d12c0d4fd0a2b
sagar <sagarkonda596@gmail.com>
2020-05-18 23:35:59 +0530

    Node HTTP Example 2

--- course4/node_http/index.js
+++ course4/node_http/index.js
@@ -1,11 +1,56 @@
 const http=require('http')
+const fs=require('fs')
+const path=require('path')
+
 const hostname='localhost'
 const port=3000
+
+
 const server=http.createServer((req,res)=>{
-    console.log(req.headers)
+    console.log(`Request For ${req.url} Method ${req.method}`)
     res.statusCode=200
-    res.setHeader('Content-Type','text/html')
-    res.end('<html><body><h1>Hello World</h1></body></html>')
+    if(req.method=='GET')
+    {
+        var fileUrl;
+        if(req.url=='/')
+            fileUrl='/index.html'
+        else
+            fileUrl=req.url
+        var filePath=path.resolve('./public'+fileUrl)
+        const  fileExt=path.extname(filePath)
+        if(fileExt=='.html')
+        {
+            fs.exists(filePath,(exists)=>
+            {
+                if(!exists)
+                {
+                    res.statusCode=404
+                    res.setHeader('Content-Type','text/html')
+                    res.end(`<html><body><h1>Err 404 : ${fileUrl} Not Found  </h1></body></html>`)
+                    return
+                }
+                else
+                {
+                    res.setHeader('Content-Type','text/html')
+                    fs.createReadStream(filePath).pipe(res)
+
+                }
+            })
+        }
+        else
+        {
+            res.statusCode=404
+            res.setHeader('Content-Type','text/html')
+            res.end(`<html><body><h1>Err 404 : ${fileUrl} Not an HTML File  </h1></body></html>`)
+            return  
+        }
+    }
+    else{
+        res.statusCode=404
+        res.setHeader('Content-Type','text/html')
+        res.end(`<html><body><h1>Err 404 : ${req.method} not Supported  </h1></body></html>`)
+        return  
+    }
 })
 server.listen(port,hostname,()=>{
     console.log(`Server Running at http://${hostname}:${port}`)
