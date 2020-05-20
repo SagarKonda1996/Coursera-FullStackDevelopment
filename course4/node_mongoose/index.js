78b87575c797561a48edccaee3740de8c8ce51fc
sagar <sagarkonda596@gmail.com>
2020-05-20 21:03:55 +0530

    course 4: Mongoose DB Schema Nested

--- course4/node_mongoose/index.js
+++ course4/node_mongoose/index.js
@@ -8,18 +8,33 @@ const connect=mongoose.connect(url);
 connect
 .then((db)=>{
     console.log("Connected Correctly to Server")
-    var newDish=Dishes({
+    Dishes.create({
         name:"Uthappizza",
         description:"test"
     })
-    newDish.save()
+
     .then((dish)=>{
         console.log(dish)
 
-       return Dishes.find({}).exec()
+       return Dishes.findByIdAndUpdate(dish._id,
+        {
+          $set:{
+            description:"Updated Test"}
+        },
+          {
+              new:true
+        }).exec()
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish)=>{
+        dish.comments.push({
+            rating:5,
+            comment:`I'm getting a Sinking Feeling !`,
+            author:"Leornado di Carpaccio"
+        })
+        return dish.save()
+    })
+    .then((dish)=>{
+        console.log(dish)
         return Dishes.deleteMany({})
     })
     .then(()=>{
