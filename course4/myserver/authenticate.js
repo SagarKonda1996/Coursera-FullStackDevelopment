8e8f16203de110c3caac1a075bf769a8a6a78d2b
sagar <sagarkonda596@gmail.com>
2020-05-22 17:07:08 +0530

    Course 4: Assignment 3->Admin priveleges

--- course4/myserver/authenticate.js
+++ course4/myserver/authenticate.js
@@ -1,23 +1,23 @@
-var passport=require('passport');
-var LocalStrategy=require('passport-local').Strategy;
-var User=require('./models/users');
-var JwtStrategy=require('passport-jwt').Strategy;
-var ExtractJwt=require('passport-jwt').ExtractJwt;
-var jwt=require('jsonwebtoken');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/users');
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var jwt = require('jsonwebtoken');
 
-var config=require('./config');
+var config = require('./config');
 
-exports.local=passport.use(new LocalStrategy(User.authenticate()));
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken=(user)=>{
-    return jwt.sign(user,config.secretKey,
-            {expiresIn:3600});
+exports.getToken = (user) => {
+    return jwt.sign(user, config.secretKey,
+        { expiresIn: 3600 });
 }
-var opts={}
-opts.jwtFromRequest=ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey=config.secretKey;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
     (jwt_payload, done) => {
@@ -36,4 +36,13 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 
-exports.verifyUser=passport.authenticate('jwt',{session:false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin && req.user.admin == true) {
+        return next()
+    }
+    err = new Error(`You are Not Authorized to Perform This Action`)
+    err.status = 403;
+    return next(err)
+}
